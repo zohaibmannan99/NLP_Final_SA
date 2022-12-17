a45abd2461dcaebe040078a71ece8761a79e50f3
Nico Bykhovsky <nicobykhovsky@10-16-31-89.dynapool.wireless.nyu.edu>
2022-12-17 15:56:44 -0500

    Fixed LSTM optimizer function, training, and sizing

--- lstm.py
+++ lstm.py
@@ -46,10 +46,9 @@ class LSTM:
         y = torch.matmul(self.Wy, h.t()) + self.by
         
         return h, c, y
-    
+
 def sigmoid(x):
     return 1 / (1 + torch.exp(-x))
-
 
 
 #USE LSTM CLASS
@@ -99,15 +98,14 @@ hidden_size = 128
 output_size = 3
 model = LSTM(input_size, hidden_size, output_size)
 
-# define the loss function and the optimizer
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+# define the criterion and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
 
-# define the hyperparameters
-batch_size = 32
+# training loop
 num_epochs = 10
+batch_size = 2
 
-# train the model
 for epoch in range(num_epochs):
     # shuffle the data
     permutation = torch.randperm(X_train.size()[0])
@@ -123,4 +121,40 @@ for epoch in range(num_epochs):
         
         # initialize the hidden state and cell state for the LSTM
         h_prev = torch.zeros((hidden_size, 1))
-        c_prev = torch.zeros((hidden))
+        c_prev = torch.zeros((hidden_size, 1))
+        
+        # zero the gradients of the model parameters
+        optimizer.zero_grad()
+        
+        # forward pass
+        h, c, y_pred = model(X_batch, h_prev, c_prev)
+        
+        # compute the loss
+        loss = criterion(y_pred.t(), y_batch)
+        
+        # backward pass
+        loss.backward()
+        
+        # update the model parameters
+        optimizer.step()
+
+# test the model
+h_prev = torch.zeros((hidden_size, 1))
+c_prev = torch.zeros((hidden_size, 1))
+y_pred = model(X_test, h_prev, c_prev)
+predictions = y_pred.argmax(dim=1)
+accuracy = (predictions == y_test).float().mean()
+print(f'Test accuracy: {accuracy:.2f}')
+
+
+
+
+
+
+#To use an optimizer and prevent overfitting, you can follow these steps:
+#Import the optimizer module from PyTorch, e.g. from torch import optim.
+#Create an optimizer object, passing in the model's parameters as an argument, e.g. optimizer = optim.SGD(model.parameters(), lr=0.01).
+#Before each training iteration, reset the gradients to zero by calling optimizer.zero_grad().
+#After computing the loss, compute the gradients of the loss with respect to the model's parameters by calling loss.backward().
+#Update the model's parameters by calling optimizer.step().
+#You may also want to consider using techniques such as dropout or weight decay to prevent overfitting and improve the generalization of the model. You can do this by adding dropout layers to the model or setting the weight_decay argument when creating the optimizer.
